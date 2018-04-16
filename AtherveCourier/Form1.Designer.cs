3b295eeba768b13f4eaf5c0b7404696459e54af8
prerna <prerna@gmail.com>
2018-04-16 11:55:49 +0530

    first commit

---
+++ AtherveCourier/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace AtherveCourier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_EmployeeId = new System.Windows.Forms.Label();
+            this.txt_EmployeeID = new System.Windows.Forms.TextBox();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_EmployeeId
+            // 
+            this.lbl_EmployeeId.AutoSize = true;
+            this.lbl_EmployeeId.Location = new System.Drawing.Point(329, 112);
+            this.lbl_EmployeeId.Name = "lbl_EmployeeId";
+            this.lbl_EmployeeId.Size = new System.Drawing.Size(64, 13);
+            this.lbl_EmployeeId.TabIndex = 0;
+            this.lbl_EmployeeId.Text = "EmployeeID";
+            // 
+            // txt_EmployeeID
+            // 
+            this.txt_EmployeeID.Location = new System.Drawing.Point(454, 104);
+            this.txt_EmployeeID.Name = "txt_EmployeeID";
+            this.txt_EmployeeID.Size = new System.Drawing.Size(192, 20);
+            this.txt_EmployeeID.TabIndex = 1;
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Location = new System.Drawing.Point(454, 481);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(75, 23);
+            this.btn_submit.TabIndex = 2;
+            this.btn_submit.Text = "Submit";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1486, 613);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.txt_EmployeeID);
+            this.Controls.Add(this.lbl_EmployeeId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_EmployeeId;
+        private System.Windows.Forms.TextBox txt_EmployeeID;
+        private System.Windows.Forms.Button btn_submit;
+    }
+}
+
---
+++ AtherveCourier/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtherveCourier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            string Employeeid = txt_EmployeeID.Text;
+
+            SqlConnection con = new SqlConnection("Data Source=USHYDPRERNGUPT1\\SQLEXPRESS;Initial Catalog=testConnectionString;Integrated Security=True");
+            SqlCommand cmd;
+            con.Open();
+            string s = "insert into AtherveEmployee values(@p1)";
+            cmd = new SqlCommand(s, con);
+            cmd.Parameters.AddWithValue("@p1", Employeeid);
+            //cmd.Parameters.AddWithValue("@p2", txtSName.Text);
+            //cmd.Parameters.AddWithValue("@p3", txtSCourse.Text);
+            cmd.CommandType = CommandType.Text;
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show(i + " Row(s) Inserted ");
+            
+        }
+    }
+}
